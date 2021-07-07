7d524323dd79a79e6c35d572f670f0e5649d5954
Kitajima2910 <kitajima2910@gmail.com>
2021-07-07 22:51:18 +0700

    add cloudinary, fix code, usercontroller

--- backend/src/main/java/com/javadocfast/crud/payload/response/JwtResponse.java
+++ backend/src/main/java/com/javadocfast/crud/payload/response/JwtResponse.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -17,15 +18,37 @@ public class JwtResponse {
     private int jwtExpirationMs;
     private long id;
     private String username;
+    private String fullName;
+    private Date birthday; // 1995-10-29
+    private int gender; // 0: Nam, 1: Nữ, 3: Khác
+    private String address;
+    private String postcode;
+    private String phone;
+    private String linkImage;
+    private String nameImage;
     private String email;
     private List<String> roles;
+    private Date createdAt;
+    private Date updatedAt;
 
-    public JwtResponse(String accessToken, int jwtExpirationMs, long id, String username, String email, List<String> roles) {
-        this.token = accessToken;
+    public JwtResponse(String token, int jwtExpirationMs, long id, String username, String fullName,
+                       Date birthday, int gender, String address, String postcode, String phone, String linkImage,
+                       String nameImage, String email, List<String> roles, Date createdAt, Date updatedAt) {
+        this.token = token;
         this.jwtExpirationMs = jwtExpirationMs;
         this.id = id;
         this.username = username;
+        this.fullName = fullName;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.address = address;
+        this.postcode = postcode;
+        this.phone = phone;
+        this.linkImage = linkImage;
+        this.nameImage = nameImage;
         this.email = email;
         this.roles = roles;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 }
